f1d42a0d756b8295e202db0d6115a47da1f1adee
oz <130259602+powchita@users.noreply.github.com>
2023-07-20 11:57:12 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,24 @@
+#lista = [1, 2.5, "Oz Menezes", True]
+
+#print(lista[0])
+#print(lista[3])
+
+#lista[1] = 12
+
+#if 12 in lista : se estiver na posição 12 da lista 
+#len: tamanho de algo  ex:
+
+time = input("Qual é o maior time de futebol Pernambucano? ")
+list = ["Sport", "Náutico", "Santa Cruz"]
+
+if time == list[0]:
+    print("É o Sport!!")
+    
+elif time == list[1]:
+    print("É o Náutico!!")
+    
+elif time == list[2]:
+    print("É o Santa Cruz!!")
+    
+else:
+    print("Te lasca então")    

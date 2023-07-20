8896a79549092e5481e817fb4a64ce6e064efec3
oz <130259602+powchita@users.noreply.github.com>
2023-07-20 11:56:19 -0300

    Create aula.py

---
+++ aula.py
@@ -0,0 +1,10 @@
+numero_um = input("Por favor, digite um número: ")
+numero_um = int(numero_um)
+
+numero_dois = input("Agora, digite outro número: ")
+numero_dois = int(numero_dois)
+
+print(numero_um * numero_dois)
+
+seu_nome = input("Qual é o seu nome? ")
+print(seu_nome + " se garante")
